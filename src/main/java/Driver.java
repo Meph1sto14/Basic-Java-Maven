f8b24c94bb1956fd997ec4d4764809ad8973e815
Marvel Sanjaya Setiawan <marvelsanjaya7@gmail.com>
2026-05-14 12:27:25 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,15 +7,30 @@ public class Driver {
 		Counter counter = new Counter();
 		
 		System.out.println("Count:" + counter.getCount());
-		
+		if (counter.getCount() == 0) {
+			System.out.println("PASS: Initial value benar");
+		} else {
+			System.out.println("FAIL: Initial value salah");
+		}
+
 		counter.increment();
-		
+
 		System.out.println("Count:" + counter.getCount());
+		if (counter.getCount() == 1) {
+			System.out.println("PASS: Increment berhasil");
+		} else {
+			System.out.println("FAIL: Increment gagal");
+		}
 		
 		counter.decrement();
 		
 		System.out.println("Count:" + counter.getCount());
-		
+		if (counter.getCount() == 0) {
+			System.out.println("PASS: Decrement berhasil");
+		} else {
+			System.out.println("FAIL: Decrement gagal");
+		}
+
 	}
 
 }
