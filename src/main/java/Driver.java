d05573742e71933d434d9502f0356054f3051f6f
Marvel Sanjaya Setiawan <marvelsanjaya7@gmail.com>
2026-05-23 11:17:36 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,31 +6,29 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count:" + counter.getCount());
-		if (counter.getCount() == 0) {
-			System.out.println("PASS: Initial value benar");
-		} else {
-			System.out.println("FAIL: Initial value salah");
-		}
-
-		counter.increment();
-
-		System.out.println("Count:" + counter.getCount());
-		if (counter.getCount() == 1) {
-			System.out.println("PASS: Increment berhasil");
-		} else {
-			System.out.println("FAIL: Increment gagal");
-		}
+		// System.out.println("Count:" + counter.getCount());
 		
-		counter.decrement();
+		// counter.increment();
+		
+		// System.out.println("Count:" + counter.getCount());
+		
+		// counter.decrement();
+		
+		// System.out.println("Count:" + counter.getCount());
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.increseBy(5);
 		
 		System.out.println("Count:" + counter.getCount());
-		if (counter.getCount() == 0) {
-			System.out.println("PASS: Decrement berhasil");
-		} else {
-			System.out.println("FAIL: Decrement gagal");
-		}
-
+		
+		counter.decreseBy(2);
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.multiplyBy(6);
+		
+		System.out.println("Current count:" + counter.getCount());
 	}
 
 }
